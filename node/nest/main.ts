413c931108a3275ff30616257811c9e2b235b929
v_yvywang <y450150@163.com>
2022-02-13 22:41:30 +0800

    fix: cors | port

--- node/nest/main.ts
+++ node/nest/main.ts
@@ -1,10 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  await app.listen(8081);
+  const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api')
+
+  app.enableCors({
+    origin: false
+      ? function (origin, callback) {
+          const whitelist = []
+          if (whitelist.includes(origin) || !origin) {
+            callback(null, true)
+          } else {
+            callback(new Error(`${origin} Not allowed by CORS`))
+          }
+        }
+      : true,
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  })
+
+  await app.listen(8080)
 }
 
-bootstrap();
+bootstrap()
